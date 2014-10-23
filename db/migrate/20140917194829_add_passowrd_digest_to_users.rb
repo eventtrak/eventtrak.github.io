80739bbd172d20110ec6d1462eff4401791ac0dc
ifilondz@gmail.com <cornelldatascience@gmail.com>
2014-10-22 22:04:12 -0400

    tore it apart

--- db/migrate/20140917194829_add_passowrd_digest_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPassowrdDigestToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :passowrd_digest, :string
-  end
-end
--- db/migrate/20140917195303_fix_password_digest_spelling.rb
+++
@@ -1,5 +0,0 @@
-class FixPasswordDigestSpelling < ActiveRecord::Migration
-  def change
-    rename_column :users, :passowrd_digest, :password_digest
-  end
-end
