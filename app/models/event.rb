128afdb64969b3694c52f6b8603e2e4cdc259bf1
Ilan Filonenko <ifilondz@gmail.com>
2014-11-23 19:04:51 -0500

    landingpage

--- app/models/event.rb
+++
@@ -1,4 +0,0 @@
-class Event < ActiveRecord::Base
-  validates_presence_of :title
-  validates_presence_of :body
-end
--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  root 'events#test'
-  resources :events
-end
